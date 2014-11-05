3dda1c3fff647c79089db630e82196f57631e1f7
GFORTI <GFORTI@N242INSTR.labs.neit.edu>
2014-11-05 17:56:30 -0500

    week5

---
+++ week5/demo/textfields.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+       
+        <form action="#" method="post">
+            
+            
+<label>Text</label>
+<input type="text" name="textfield" value="" /><br />
+    
+<label>Text</label>
+<input type="password" name="passwordfield" value="" /><br />
+
+<input type="hidden" name="hiddenfield" value="" /><br />
+            
+            
+            
+        </form>
+        
+    </body>
+</html>

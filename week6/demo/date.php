26c0c75f7de8037a2906286b78ce07173a940832
GFORTI <GFORTI@N242INSTR.labs.neit.edu>
2014-11-12 18:40:30 -0500

    week6

---
+++ week6/demo/date.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+            $today = date('m-d-y');
+            $time = time();
+            
+            
+            $str2time = strtotime('11/12/14');
+            
+            echo date($time);
+            
+            
+        
+        
+        
+        ?>
+    </body>
+</html>

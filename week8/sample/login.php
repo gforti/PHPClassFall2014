e29769b9a1aa316faf952d5ada74b4946394262f
GFORTI <GFORTI@N242INSTR.labs.neit.edu>
2014-12-03 18:16:21 -0500

    sessions

---
+++ week8/sample/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <form action="#" name="mainform">        
+            Passcode: <input name="passcode" value="" />        
+        </form>
+        
+        
+        <?php
+        // put your code here
+        
+        
+        
+        
+        ?>
+    </body>
+</html>

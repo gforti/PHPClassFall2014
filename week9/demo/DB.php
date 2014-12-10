f0823d6bd88717d1c18d618f914556e08ae68675
GFORTI <GFORTI@N242INSTR.labs.neit.edu>
2014-12-10 18:42:45 -0500

    classes

--- week9/demo/DB.php
+++ week9/demo/DB.php
@@ -15,6 +15,17 @@ class DB {
     
     private $PDO = null;
     
-    
-    
+    function __construct() {
+        $this->setPDO();
+    }
+
+    public function getPDO() {
+        return $this->PDO;
+    }
+
+    private function setPDO() {
+        $this->PDO = new PDO("mysql:host=localhost;dbname=phpclassfall2014", "root", "");
+    }
+
+
 }
